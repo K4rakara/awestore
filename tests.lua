8263919c1448fd107cdcfc19f25456e2e0765a4d
Jack Johannesen <jack@insertdomain.name>
2021-03-14 18:27:17 -0400

    Make tests more complete.

--- tests.lua
+++ tests.lua
@@ -7,7 +7,7 @@ local test = (function(name, fn)
   if not ok then
     print("\x1b[1A\r\x1b[K\x1b[1;38;5;1m        Failed \x1b[0m"..name)
     local pl = (function()
-      local ok, pl = pcall(require("pl.pretty"))
+      local ok, pl = pcall(require, "pl.pretty")
       if not ok then pl = (function(value) print(value) end) end
       return pl
     end)()
@@ -17,30 +17,64 @@ local test = (function(name, fn)
   end
 end)
 
-test("writable:subscribe,set", function()
+test("subscribe,set", function()
   local i = 0
   local store = awestore.writable(0)
-  
   assert(i == 0)
-  
   store:subscribe(function(_) i = i + 1 end)
-  
   assert(i == 1)
-  
   store:set(1)
-  
   assert(i == 2)
-  
   local got
-  
   store:subscribe(function(v) got = v end)
-  
   assert(i == 2)
   assert(got == 1)
-  
   store:set(2)
-  
   assert(i == 3)
   assert(got == 2)
+end)
+
+test("get", function()
+  local store = awestore.writable(0)
+  assert(store:get() == 0)
+  store:set(1)
+  assert(store:get() == 1)
+end)
+
+test("update", function()
+  local got
+  local store = awestore.writable(0) 
+  store:subscribe(function(v) got = v end)
+  assert(got == 0)
+  store:update(function(v) return v + 1 end)
+  assert(got == 1)
+end)
+
+test("derive", function()
+  local store = awestore.writable(0)
+  local derive = store:derive(function(v) return v ^ v end)
+  local got
+  derive:subscribe(function(v) got = v end)
+  assert(got == 1)
+  store:set(1)
+  assert(got == 1)
+  store:set(2)
+  assert(got == 4)
+  store:set(3)
+  assert(got == 27)
+end)
+
+test("monitor", function()
+  local store = awestore.writable(0)
+  local monitor = store:monitor()
+  local got
+  monitor:subscribe(function(v) got = v end)
+  assert(got == 0)
+  store:set(1)
+  assert(got == 1)
+  store:set(2)
+  assert(got == 2)
+  store:set(3)
+  assert(got == 3)
 end)
 
