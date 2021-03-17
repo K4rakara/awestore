742cbe619a44f6f2466fa7c36ca4c96a635fc2e6
Jack Johannesen <jack@insertdomain.name>
2021-03-16 21:22:54 -0400

    Refactor core stores

--- src/core.lua
+++ src/core.lua
@@ -1,157 +1,153 @@
-local utils = require("awestore.utils")
+local utils = require "awestore.utils"
 
-local subscribe, store, readable, writable, monitor, derived
-
-subscribe = (function(store, ...)
-  if store == nil then return utils.noop end
-  local unsub = store:subscribe(...)
-  return unsub.unsubscribe
-    and (function() return unsub:unsubscribe(); end)
-     or unsub
-end)
+local derived, filtered, monitored, readable, signal, store, writable
 
 store = setmetatable({ }, {
   __name = "store",
-  __tostring = (function(self)
-    return "store"
-  end),
-  __newindex = (function(self, key, value) end),
+  __tostring = function(self) return "store"; end,
+  __newindex = function(self, key, value) return; end,
 })
 
-readable = setmetatable({
-  __index = false,
-  __tostring = (function(self)
-    return "readable: "..tostring(self:get())
-  end),
-  derive = (function(self, fn)
-    return derived(self, fn)
-  end),
-  get = (function(self)
+function writable(value, start)
+  local self = { [store] = true, [readable] = true, [writable] = true, }
+  local value = value
+  local subscribers = { }
+  local start, stop = start or utils.noop, nil
+  
+  function self:set(new_value)
+    if value ~= new_value then
+      value = new_value
+      if stop ~= nil then
+        for _, subscriber in ipairs(subscribers) do
+          subscriber(value)
+        end
+      end
+    end
+  end
+  
+  function self:subscribe(fn)
+    subscribers[#subscribers + 1] = fn
+    if #subscribers == 1 then
+      stop = (start(function(value) self:set(value); end)) or utils.noop
+    end
+    fn(value)
+    return function()
+      for i, fn_ in ipairs(subscribers) do
+        if fn_ == fn then
+          table.remove(subscribers, i)
+          break
+        end
+      end
+      if #subscribers == 0 then stop(); stop = nil; end
+    end
+  end
+  
+  function self:update(fn) self:set(fn(value)); end
+  
+  function self:derive(fn) return derived(self, fn); end
+  
+  function self:get()
     local a
     self:subscribe(function(b) a = b end)()
     return a
-  end),
-  monitor = (function(self)
-    return monitor(self)
-  end),
-}, {
-  __index = store,
-  __call = (function(self, value, start)
-    return setmetatable({
-      [store] = true,
-      [readable] = true,
-      subscribe = writable(value, start).subscribe,
-    }, self)
-  end),
-  __name = "readable",
-  __tostring = (function(self)
-    return "readable"
-  end),
-})
+  end
+  
+  function self:monitor() return monitored(self); end 
+  
+  function self:filter(fn) return filtered(self, fn); end
+  
+  return self
+end
 
-readable.__index = readable
-
-writable = setmetatable({
-  __index = false,
-  __tostring = (function(self)
-    return "writable: "..tostring(self:get())
-  end),
-}, {
-  __index = readable,
-  __call = (function(self, value, start)
-    local value = value
-    local start, stop = start or utils.noop, nil
-    local subscribers = { }
-    local update, set, subscribe
-    update = (function(self, fn) set(self, fn(value)) end)
-    set = (function(self, new_value)
-      if value ~= new_value then
-        value = new_value
-        if stop ~= nil then
-          for _, subscriber in ipairs(subscribers) do
-            subscriber(value)
-          end
-        end
+function signal()
+  local self = { [store] = true, [readable] = true, [signal] = true, }
+  local subscribers = { }
+  
+  function self:fire()
+    for _, subscriber in ipairs(subscribers) do fn(); end
+  end
+  
+  function self:subscribe(fn)
+    subscribers[#subscribers + 1] = fn
+    return function()
+      for i, fn_ in ipairs(subscribers) do
+        if fn_ == fn then table.remove(subscribers, i); end
+        break
       end
+    end
+  end
+  
+  function self:get() return; end
+  
+  function self:monitor() return monitored(self); end
+  
+  function self:derive(fn) return derived(self, fn); end
+  
+  function self:filter(fn) return filtered(self, fn); end
+  
+  return self
+end
+
+function readable(value, start)
+  local self = { [store] = true, [readable] = true, }
+  local inner = writable(value, start)
+  
+  function self:subscribe(fn) return inner:subscribe(fn); end
+  
+  function self:derive(fn) return derived(self, fn); end
+  
+  function self:get()
+    local a
+    self:subscribe(function(b) a = b end)()
+    return a
+  end
+  
+  function self:monitor() return monitored(self); end
+  
+  function self:filter(fn) return filtered(self, fn); end
+  
+  return self
+end
+
+function monitored(store_)
+  local self = readable(nil, function(set)
+    return store_:subscribe(function(value) set(value); end)
+  end)
+  
+  self[monitored] = true
+  
+  return self
+end
+
+function derived(store_, fn)
+  local self = readable(nil, function(set)
+    return store_:subscribe(function(value) set(fn(value)); end)
+  end)
+  
+  self[derived] = true
+  
+  return self
+end
+
+function filtered(store_, fn)
+  local self = readable(nil, function(set)
+    return store_:subscribe(function(value)
+      if fn(value) then set(value); end
     end)
-    subscribe = (function(self, fn)
-      subscribers[#subscribers + 1] = fn
-      if #subscribers == 1 then stop = (start(set)) or utils.noop end
-      fn(value)
-      return (function()
-        for i, fn_ in ipairs(subscribers) do
-          if fn_ == fn then
-            table.remove(subscribers, i)
-            break
-          end
-        end
-        if #subscribers == 0 then stop(); stop = nil; end
-      end)
-    end)
-    return setmetatable({
-      [store] = true,
-      [readable] = true,
-      [writable] = true,
-      set = set,
-      update = update,
-      subscribe = subscribe,
-    }, self)
-  end),
-  __name = "writable",
-  __tostring = (function(self)
-    return "writable"
-  end),
-})
-
-writable.__index = writable
-
-monitor = setmetatable({  
-  __index = false,
-  __tostring = (function(self) return "monitor: "..tostring(self:get()) end),
-}, {
-  __index = readable,
-  __call = (function(self, readable)
-    return setmetatable({
-      [store] = true,
-      [readable] = true,
-      [monitor] = true,
-      subscribe = (function(self, fn) return readable:subscribe(fn) end),
-    }, self)
-  end),
-  __name = "monitor",
-  __tostring = (function(self) return "monitor" end),
-})
-
-monitor.__index = monitor
-
-derived = setmetatable({
-  __index = false,
-  __tostring = (function(self) return "derived: "..tostring(self:get()) end),
-}, {
-  __index = readable,
-  __call = (function(self, store, fn)
-    local derived = writable(nil)
-    store:subscribe(function(value) derived:set(fn(value)) end)
-    return setmetatable({
-      [derived] = true,
-      [store] = true,
-      [readable] = true,
-      [monitor] = true,
-      subscribe = (function(self, fn) return derived:subscribe(fn) end),
-    }, self)
-  end),
-  __name = "derived",
-  __tostring = (function(self) return "derived" end),
-})
-
-derived.__index = derived
+  end)
+  
+  self[filtered] = true
+  
+  return self
+end
 
 return {
   derived = derived,
-  monitor = monitor,
+  filtered = filtered,
+  monitored = monitored,
   readable = readable,
+  signal = signal,
   store = store,
-  subscribe = subscribe,
   writable = writable,
 }
 
